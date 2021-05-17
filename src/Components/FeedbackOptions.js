2aa5aaf8702535f7af9fef608836ec177768821d
Luhivskyi <luhivskyi.d@ukr.net>
2021-05-17 20:34:59 +0300

    preFinal

---
+++ src/Components/FeedbackOptions.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const FeedbackOptions = ({ options, handleClickButton }) => (
+    
+    <>
+        {options.map(elem => (
+
+            <button key="elem" onClick={() => handleClickButton(elem)}>{elem }</button>
+            
+        ))}
+    </>
+
+
+)
+
+export default FeedbackOptions;
